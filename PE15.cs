dc13dce8ed43586433656c857a544fbbb95bb1d2
Uttam <45071838+Uttam1225@users.noreply.github.com>
2024-12-18 18:15:45 +0530

    Create PE15.cs

---
+++ PE15.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+namespace EulerProject
+{
+    public class LatticePaths
+    {
+        //PE: Problem 15
+        public static void Solve()
+        {
+            BigInteger fact = 1;
+            BigInteger Denomfact = 1;
+
+            for(int i=2;i<=40;++i)
+            {
+                fact*=i;
+                if(i==20)
+                {
+                    Denomfact = fact;
+                }
+            }
+            Denomfact*=Denomfact;
+
+            Console.WriteLine("result = " + fact/Denomfact);
+
+        }
+    }
+}
